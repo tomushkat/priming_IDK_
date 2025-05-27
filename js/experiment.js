a1a8aeef2fe85dc2f78a9ee34b3feb56f8a4b49e
Tom Mushkat <62655326+tomushkat@users.noreply.github.com>
2025-05-27 12:40:24 +0300

    adding queotions

--- js/experiment.js
+++ js/experiment.js
@@ -24,17 +24,25 @@ const jsPsych = initJsPsych({
 // -------------------------------
 const welcome_screen = {
   type: jsPsychHtmlKeyboardResponse,
-  stimulus: '<div style="font-size:30px;">Welcome to the experiment</div>',
+  stimulus: `
+    <div style="font-size:30px;font-weight: bold;">
+      Welcome to the experiment
+    </div>
+  `,
   choices: "NO_KEYS", 
   trial_duration: 2000
 
 };
 
+
+// -------------------------------
+// ID
+// -------------------------------
 const enter_id_screen = {
   type: jsPsychSurveyText,
   questions: [
     {
-      prompt: "Please enter your ID:",
+      prompt: "Thank you for your willingness to participate in our experiment. Please enter your prolific ID.",
       name: 'participant_id',
       required: true
     }
@@ -42,18 +50,49 @@ const enter_id_screen = {
   button_label: "Proceed to the experiment"
 };
 
+// -------------------------------
+// CONSENT
+// -------------------------------
 const consent_screen = {
+  // Use the HTML + button response plugin
   type: jsPsychHtmlButtonResponse,
+
+  // HTML content displayed to the participant
   stimulus: `
-    <div style="font-size:22px; margin-bottom:30px;">
-      Please read the participation rules and mark that you agree to continue.
+    <!-- Wrapper div with overall font size, spacing, and left alignment -->
+    <div style="font-size:22px; margin-bottom:30px; text-align: left;">
+
+      <!-- Centered and bold title -->
+      <div style="text-align: center; font-weight: bold; font-size: 26px; margin-bottom: 20px;">
+        Consent to Participate in Research
+      </div>
+
+      <!-- Main body text with bold section headers using <strong> -->
+      You are invited to participate in a research study.<br><br>
+
+      <strong>Duration:</strong> The study should last about 5 minutes.<br>
+      <strong>Risks:</strong> There are no physical or emotional risks involved.<br>
+      <strong>Confidentiality:</strong> Your data will be recorded, analyzed, and kept on file for the sake of possible future analysis.<br>
+      We will do our best to maintain confidentiality by keeping your data under lock, and by separating your identity from the data when coding and analyzing them so that others will not be able to connect you with your data.<br>
+      <strong>Your rights:</strong> Your participation is voluntary and you have the right to withdraw your consent or discontinue participation at any time. Your privacy will be maintained in all published and written data resulting from this study.<br><br>
+
+      <!-- Final instruction -->
+      For more information, you can email tom.mushkat@mail.huji.ac.il.<br>
+      Click on the "I agree" button to participate.
     </div>
   `,
+
+  // Button options — single choice: "I agree"
   choices: ['I agree'],
-  button_html: (choice) => `<button class="jspsych-btn" style="font-size:20px; padding:12px 24px;">${choice}</button>`
+
+  // Custom button appearance (padding, font size)
+  button_html: (choice) =>
+    `<button class="jspsych-btn" style="font-size:20px; padding:12px 24px;">${choice}</button>`
 };
 
-
+// -------------------------------
+// Instructions
+// -------------------------------
 const instructions = {
   type: jsPsychHtmlButtonResponse,
   stimulus: `
@@ -71,7 +110,7 @@ const instructions = {
 // Define global variables
 // -------------------------------
 const trials = [];
-const total_trials = 12;
+const total_trials = 6;
 
 let conditions = Array(total_trials / 2).fill('ambiguous').concat(Array(total_trials / 2).fill('risky'));
 conditions = jsPsych.randomization.shuffle(conditions);
@@ -187,7 +226,192 @@ for (let i = 0; i < total_trials; i++) {
   });
 }
 
+
+// -------------------------------
+// answer qeustions screen
+// -------------------------------
+const answer_questions = {
+  type: jsPsychHtmlKeyboardResponse,
+  stimulus: '<div style="font-size:30px;">Please answer the following questions</div>',
+  choices: "NO_KEYS", 
+  trial_duration: 2000
+
+};
+
+// -------------------------------
+// Honesty Check
+// -------------------------------
+const Honesty = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
+    <div style="font-size:22px; margin-bottom:30px;">
+      In your honest opinion, should we use your data in our analysis?<br> This is not related to how well you performed, and will not affect your payment for participation, but whether you put in a reasonable effort.
+    </div>
+  `,
+  choices: ['Yes, I put in a reasonable effort', 'Maybe, I was a little distracted', "No, I really wasn't paying attention"],
+  button_html: (choice) =>
+    `<button class="jspsych-btn" style="font-size:20px; padding:12px 24px; margin: 6px;">${choice}</button>`,
+  on_finish: function(data) {
+    // You can use data.response to handle branching logic later if needed
+    console.log('Consent choice:', data.response); // 0, 1, or 2
+  }
+};
+
+// -------------------------------
+// Consecutively Check
+// -------------------------------
+const Consecutively = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
+    <div style="font-size:22px; margin-bottom:30px;">
+      Did you complete the experiment consecutively?
+    </div>
+  `,
+  choices: ['Yes', "No"],
+  button_html: (choice) =>
+    `<button class="jspsych-btn" style="font-size:20px; padding:12px 24px; margin: 6px;">${choice}</button>`,
+  on_finish: function(data) {
+    // You can use data.response to handle branching logic later if needed
+    console.log('Consent choice:', data.response); // 0, 1, or 2
+  }
+};
+
+// -------------------------------
+// Disturbances Check
+// -------------------------------
+const Disturbances = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
+    <div style="font-size:22px; margin-bottom:30px;">
+      While completing the experiment, did you experience any external disturbances?
+    </div>
+  `,
+  choices: ['Yes', "No"],
+  button_html: (choice) =>
+    `<button class="jspsych-btn" style="font-size:20px; padding:12px 24px; margin: 6px;">${choice}</button>`,
+  on_finish: function(data) {
+    // You can use data.response to handle branching logic later if needed
+    console.log('Consent choice:', data.response); // 0, 1, or 2
+  }
+};
+
+// -------------------------------
+// Alone Check
+// -------------------------------
+const Alone = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
+    <div style="font-size:22px; margin-bottom:30px;">
+      Did you fill in this experiment alone or with someone else?
+    </div>
+  `,
+  choices: ['Alone', "With someone else"],
+  button_html: (choice) =>
+    `<button class="jspsych-btn" style="font-size:20px; padding:12px 24px; margin: 6px;">${choice}</button>`,
+  on_finish: function(data) {
+    // You can use data.response to handle branching logic later if needed
+    console.log('Consent choice:', data.response); // 0, 1, or 2
+  }
+};
+
+// -------------------------------
+// Purpose
+// -------------------------------
+const Purpose = {
+  type: jsPsychSurveyText,
+  questions: [
+    {
+      prompt: "What do you think was the purpose of the study?",
+      name: 'study_purpose',
+      required: true
+    }
+  ],
+  button_label: "Proceed to the experiment"
+};
+
+
+// -------------------------------
+// demographics
+// -------------------------------
+// -------------------------------
+// Gender
+// -------------------------------
+const gender = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
+    <div style="font-size:22px; margin-bottom:30px;">
+      Please enter your gender:
+    </div>
+  `,
+  choices: ['Man', 'Woman', 'Prefer not to say'],
+  button_html: (choice) =>
+    `<button class="jspsych-btn" style="font-size:20px; padding:12px 24px; margin: 6px;">${choice}</button>`,
+  on_finish: function(data) {
+    // You can use data.response to handle branching logic later if needed
+    console.log('Consent choice:', data.response); // 0, 1, or 2
+  }
+};
+
+// -------------------------------
+// Attention Check
+// -------------------------------
+const Attention = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
+    <div style="font-size:22px; margin-bottom:30px;">
+      Please mark "Maybe":
+    </div>
+  `,
+  choices: ['Yes', 'No', 'Maybe'],
+  button_html: (choice) =>
+    `<button class="jspsych-btn" style="font-size:20px; padding:12px 24px; margin: 6px;">${choice}</button>`,
+  on_finish: function(data) {
+    // You can use data.response to handle branching logic later if needed
+    console.log('Consent choice:', data.response); // 0, 1, or 2
+  }
+};
+
+
+// -------------------------------
+// Age
+// -------------------------------
+let valid_age = false;
+
+const enter_age_screen = {
+  type: jsPsychSurveyText,
+  questions: [
+    {
+      prompt: "Please enter your age:",
+      name: "age",
+      required: true,
+      input_type: "number",
+      textbox_columns: 5,
+      placeholder: "e.g. 25"
+    }
+  ],
+  button_label: "Proceed",
+  on_finish: function(data) {
+    const response = data.response.age;
+    const numericAge = Number(response);
+    valid_age = !isNaN(numericAge) && numericAge >= 0 && numericAge <= 100;
+  }
+};
+
+const retry_age_screen = {
+  timeline: [enter_age_screen],
+  loop_function: function() {
+    if (!valid_age) {
+      alert("Please enter your in a valid number.");
+      return true; // repeat screen
+    }
+    return false; // proceed
+  }
+};
+
 // -------------------------------
 // Start the experiment
 // -------------------------------
-jsPsych.run([welcome_screen, enter_id_screen, consent_screen, instructions, ...trials]);
+jsPsych.run([welcome_screen, enter_id_screen, consent_screen, instructions
+  , ...trials
+  , answer_questions, Honesty, Consecutively, Disturbances, Alone, Purpose
+  , gender, Attention, retry_age_screen]);
