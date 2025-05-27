2a9beda93f3e3ae43f074fa17313fb7a60ddfebd
Tom Mushkat <62655326+tomushkat@users.noreply.github.com>
2025-05-27 17:45:29 +0300

    minor changes

--- js/config.js
+++ js/config.js
@@ -1,15 +1,27 @@
 // -------------------------------
-// Set environment switch
+// Set environment variables
 // -------------------------------
 const sw_pavlovia = false;  // true = deploy mode, false = local test
+const prolific_redirect_url = "example.com";
 
 // -------------------------------
 // Experiment Settings
 // -------------------------------
 const total_trials = 6;           // Total number of trials
+const part_risky_options = [10, 20, 30, 40, 60, 70, 80, 90];
+
+// -------------------------------
+// Duration settings
+// -------------------------------
 const FIXATION_DURATION = 500;     // Duration of fixation screen (in ms)
 const PRIMING_DURATION = 200;      // Duration of priming screen (in ms)
 const Trial_DURATION = 10000;      // Duration of a trial (in ms)
 const answer_qeustions_screen_duration = 2000;
+
+// -------------------------------
+// Demographic settings
+// -------------------------------
+const age_min = 18;                // Minimum age
+const age_max = 100;               //   Maximum age
 
 
