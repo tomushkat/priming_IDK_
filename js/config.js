f7d15c72276129ba82b85d7206e6962142cc5ea3
Tom Mushkat <62655326+tomushkat@users.noreply.github.com>
2025-06-11 21:36:49 +0300

    increasing the priming to 300ms from 200ms

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // -------------------------------
 // Set environment variables
 // -------------------------------
-const sw_pavlovia = false;  // true = deploy mode, false = local test
+const sw_pavlovia = true;  // true = deploy mode, false = local test
 const prolific_redirect_url = "https://app.prolific.com/submissions/complete?cc=C1N8TMYO";
 
 
@@ -34,7 +34,7 @@ const part_risky_options = [1, 2, 3, 4, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 
 // Duration settings
 // -------------------------------
 const FIXATION_DURATION = 500;     // Duration of fixation screen (in ms)
-const PRIMING_DURATION = 200;      // Duration of priming screen (in ms)
+const PRIMING_DURATION = 300;      // Duration of priming screen (in ms)
 const Trial_DURATION = 6000;      // Duration of a trial (in ms)
 const answer_qeustions_screen_duration = 2000;
 const welcome_screen_duration = 1000
